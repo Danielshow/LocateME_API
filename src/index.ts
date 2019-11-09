6a6d663bb3001b181871cb05e19a30657eae7b8b
danielshow <danielshotonwa53@gmail.com>
2019-11-09 13:20:58 +0100

    10-set up typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({message: "it works, Yaaaaay"});
+});
+
+// handle all routes
+app.get("*", (req, res)  => {
+  res.json({message: "Routes not available"});
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("App listening on PORT 3000");
+});
